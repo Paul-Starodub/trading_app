7e0880f9778fbf641e900f81fe705cac2e5d2366
Paul Starodub <amorallex@gmail.com>
2023-05-26 23:36:53 +0300

    Updated main.py with required query params

--- main.py
+++ main.py
@@ -14,3 +14,28 @@ fake_users = [
 @app.get("/users/{user_id}")
 def get_user(user_id: int):  # str by default
     return [user for user in fake_users if user.get("id") == user_id]
+
+
+fake_trades = [
+    {
+        "id": 1,
+        "user_id": 1,
+        "currency": "BTC",
+        "side": "buy",
+        "price": 123,
+        "amount": 2.12,
+    },
+    {
+        "id": 2,
+        "user_id": 1,
+        "currency": "BTC",
+        "side": "sell",
+        "price": 125,
+        "amount": 2.12,
+    },
+]
+
+
+@app.get("/trades")
+def get_trades(limit: int, offset: int):
+    return fake_trades[offset:][:limit]
