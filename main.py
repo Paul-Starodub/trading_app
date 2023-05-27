4c024a97be183edfc98503a5f9320993ae344985
Paul Starodub <amorallex@gmail.com>
2023-05-27 10:34:19 +0300

    Updated main.py with model for user

--- main.py
+++ main.py
@@ -12,7 +12,13 @@ fake_users = [
 ]
 
 
-@app.get("/users/{user_id}")
+class User(BaseModel):
+    id: int
+    role: str
+    name: str
+
+
+@app.get("/users/{user_id}", response_model=list[User])
 def get_user(user_id: int):  # str by default
     return [user for user in fake_users if user.get("id") == user_id]
 
